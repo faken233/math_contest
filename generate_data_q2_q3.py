f01642d51888d8e9ae74120ec6615ae86c53f70e
Song He <1311471185@qq.com>
2024-09-08 11:27:52 +0800

    feat - 添加生成数据的方法 针对问题三的后一阶段
    Signed-off-by: Song He <1311471185@qq.com>

--- generate_data_q2_q3.py
+++ generate_data_q2_q3.py
@@ -30,11 +30,12 @@ def generate_matrix_q3_1(n, m):
     for zeros_positions in product([None] + list(range(m)), repeat=(n - 1)):
         matrix = np.ones((n, m), dtype=int)
 
+        # 设置前 n-1 行的零元素
         for i, pos in enumerate(zeros_positions):
             if pos is not None:
                 matrix[i, pos] = 0
 
-        # 生成最后一行
+        # 生成倒数第一行
         if m == 1:
             matrix[-1, :] = 0
             matrices.append(matrix.copy())
@@ -44,4 +45,26 @@ def generate_matrix_q3_1(n, m):
                 matrix[-1, -1] = last_row_last_element
                 matrices.append(matrix.copy())
 
+    return matrices
+
+def generate_matrix_q3_2(n):
+    # 生成所有可能的第一到第三
+    rows_1_2_3 = []
+    for i in range(n + 1):  # 0到n个0的情况
+        row = [1] * n
+        if i > 0:
+            row[i - 1] = 0
+        rows_1_2_3.append(row)
+
+    # 生成所有可能的第三行
+    rows_4 = list(itertools.product([0, 1], repeat=n))
+
+    # 组合生成所有矩阵
+    matrices = []
+    for row1 in rows_1_2_3:
+        for row2 in rows_1_2_3:
+            for row3 in rows_4:
+                for row4 in rows_4:
+                    matrix = np.array([row1, row2, row3, row4])
+                    matrices.append(matrix)
     return matrices
